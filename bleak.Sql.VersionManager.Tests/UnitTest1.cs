23464768e4f3436f29c5a0bcf087dd53e4364f15
Jamal Khan <jamal.h.khan@gmail.com>
2019-03-24 15:39:53 -0400

    working on database initialization.

--- bleak.Sql.VersionManager.Tests/UnitTest1.cs
+++ bleak.Sql.VersionManager.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 
 namespace bleak.Sql.VersionManager.Tests
@@ -7,28 +8,107 @@ namespace bleak.Sql.VersionManager.Tests
     public class UnitTest1
     {
     }
-    [TestClass]
-    public class ScriptRunning
+
+
+    public abstract class TestBaseClass
     {
-        private SqlServerVersionManager manager = new SqlServerVersionManager(
-            folder: Directory.GetCurrentDirectory(),
-            server: Configuration.Settings.Master.Server,
-            username: Configuration.Settings.Master.Username,
-            password: Configuration.Settings.Master.Password);
-        [TestMethod]
-        public void CreateDatabase()
+        protected string DatabaseName;
+        protected SqlServerVersionManager manager;
+        [TestCleanup]
+        public void Dispose()
         {
-            manager.CreateDatabase(Configuration.Settings.Master.Database);
+            manager.DropDatabase();
+        }
+
+
+        protected TestBaseClass(string testName)
+        {
+            string dbName = Configuration.Settings.Master.Database + testName + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            manager = new SqlServerVersionManager(
+                            folder: Directory.GetCurrentDirectory(),
+                            server: Configuration.Settings.Master.Server,
+                            username: Configuration.Settings.Master.Username,
+                            password: Configuration.Settings.Master.Password,
+                            databaseName: dbName,
+                            createDatabase: true
+                            );
+            DatabaseName = dbName;
         }
     }
     [TestClass]
-    public class ScriptDetection
+    public class VersionManagementTests : TestBaseClass
+    {
+        public VersionManagementTests() : base("_version_management_test_")
+        {
+        }
+    }
+
+    [TestClass]
+    public class CreateDatabaseTests : TestBaseClass
+    {
+        public CreateDatabaseTests() : base("_create_test_")
+        {
+        }
+
+        [TestMethod]
+        public void TestCreateDatabase()
+        {
+            var database = manager.GetDatabase();
+            Assert.IsTrue(database.CreateDate > DateTime.Now.AddMinutes(-5));
+            Assert.IsTrue(DateTime.Now.AddMinutes(5) > database.CreateDate);
+        }
+
+        [TestMethod]
+        public void TestDatabaseHasTables()
+        {
+            var database = manager.GetDatabase();
+            Assert.IsTrue(database.Tables.Count >= 1);
+        }
+
+        [TestMethod]
+        public void TestVersionTableCreated()
+        {
+            var database = manager.GetDatabase();
+            Assert.AreEqual(database.Tables.Count, 1);
+            Assert.IsTrue(database.Tables.Contains("Log", "version"));
+        }
+
+        [TestMethod]
+        public void TestVersionTableStructure()
+        {
+            var database = manager.GetDatabase();
+            var table = database.Tables["Log", "version"];
+            Assert.IsTrue(table.Columns.Contains("Script"));
+            Assert.IsTrue(table.Columns.Contains("DeployDate"));
+        }
+    }
+
+    [TestClass]
+    public class DropDatabaseTests : TestBaseClass
+    {
+        public DropDatabaseTests() : base("drop_database_")
+        {
+        }
+
+
+        [TestMethod]
+        public void CreateDatabase()
+        {
+            manager.DropDatabase();
+            var database = manager.GetDatabase();
+            Assert.IsNull(database);
+        }
+    }
+
+    [TestClass]
+    public class ScriptDetectionTests
     {
         private SqlServerVersionManager manager = new SqlServerVersionManager(
             folder: Directory.GetCurrentDirectory(),
             server: Configuration.Settings.Master.Server,
             username: Configuration.Settings.Master.Username,
-            password: Configuration.Settings.Master.Password);
+            password: Configuration.Settings.Master.Password,
+            databaseName: null);
         [TestMethod]
         public void TestScriptDetectionCount()
         {
--- bleak.Sql.VersionManager/Class1.cs
+++ bleak.Sql.VersionManager/Class1.cs
@@ -1,7 +1,10 @@
 ﻿using bleak.Sql.Minifier;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Linq;
 
@@ -11,43 +14,129 @@ namespace bleak.Sql.VersionManager
 
     public class bleakSqlContext : DbContext
     {
-        public MasterDbContext() : base() { }
+        public bleakSqlContext() : base() { }
         public bleakSqlContext(DbContextOptions<bleakSqlContext> options) : base(options)
         { }
         public virtual DbSet<VersionLog> VersionLogs { get; set; }
-
-        
     }
+
+    [Table("Log", Schema = "version")]
     public class VersionLog
     {
         public string ScriptName { get; set; }
-        public DateTime DeployDate { get; set; }
+        public DateTimeOffset DeployDate { get; set; }
     }
     public class SqlServerVersionManager
     {
+        public const string _VersionSchema = "version";
+        public const string _VersionTable = "log";
+
         public string Folder { get; private set; }
-        public string Server { get; private set; }
+        public string ServerInstance { get; private set; }
+        public string DatabaseName { get; private set; }
         public string Username { get; private set; }
         public string Password { get; private set; }
-
-        public SqlServerVersionManager(string folder, string server, string username, string password)
+        public ServerConnection Connection { get; private set; }
+        public Server Server { get; private set; }
+        public SqlServerVersionManager(
+            string folder,
+            string server,
+            string username,
+            string password,
+            string databaseName,
+            bool createDatabase = false
+            )
         {
             Folder = folder;
             Password = password;
             Username = username;
-            Server = server;
+            ServerInstance = server;
+            DatabaseName = databaseName;
+            Connection = new ServerConnection(serverInstance: ServerInstance, userName: Username, password: Password);
+            Server = new Server(Connection);
+            if (
+                createDatabase
+                && !string.IsNullOrEmpty(databaseName)
+                && !Server.Databases.Contains(databaseName))
+            {
+                CreateDatabase();
+            }
             DirSearch(Folder);
         }
-        public IList<Script> Scripts { get; set; } = new List<Script>();
-        public void Execute()
-        {
 
+        public IList<Script> Scripts { get; set; } = new List<Script>();
+        public void ExecuteSql(string databaseName, string sql)
+        {
+            var database = new Database(Server, databaseName);
+            database.ExecuteNonQuery(sql);
         }
 
-        public void CreateDatabase(string databaseName)
+        public Database CreateDatabase()
         {
-            database
+            var database = new Database(Server, DatabaseName);
+            database.Create();
+            IntializeDatabase();
+            return GetDatabase();
+        }
+
+        public void IntializeDatabase()
+        {
+            var database = GetDatabase();
+            if (!database.Schemas.Contains(_VersionSchema))
+            {
+                var schema = new Schema(database, "version");
+                schema.Create();
             }
+            
+            if (!database.Tables.Contains(_VersionTable, _VersionSchema))
+            {
+
+                var table = new Table(database, "Log", "version");
+
+
+                var scriptColumnName = "Script";
+                var deployDateColumnName = "DeployDate";
+                var scriptColumn = new Column(table, scriptColumnName, DataType.NVarChar(2000));
+                scriptColumn.Nullable = false;
+                table.Columns.Add(scriptColumn);
+
+                var deployDateColumn = new Column(table, deployDateColumnName, DataType.DateTimeOffset(7));
+                deployDateColumn.Nullable = false;
+                table.Columns.Add(deployDateColumn);
+
+                table.Create();
+
+                // Define Index object on the table by supplying the Table1 as the parent table and the primary key name in the constructor.  
+                Index pk = new Index(table, $"{table.Schema}_{table.Name}_PK");
+                pk.IndexKeyType = IndexKeyType.DriPrimaryKey;
+
+                // Add Col1 as the Index Column  
+                IndexedColumn idxCol1 = new IndexedColumn(pk, "Script");
+                pk.IndexedColumns.Add(idxCol1);
+
+                // Create the Primary Key  
+                pk.Create();
+            }
+            
+        }
+
+        public Database GetDatabase()
+        {
+            if (Server.Databases.Contains(DatabaseName))
+            {
+                return Server.Databases[DatabaseName];
+            }
+            return null;
+        }
+
+        public void DropDatabase()
+        {
+            var database = GetDatabase();
+            if (database != null)
+            {
+                database.Drop();
+            }
+        }
 
         private void DirSearch(string sDir)
         {
@@ -74,7 +163,6 @@ namespace bleak.Sql.VersionManager
             }
         }
     }
-
     public class Minifier
     {
         public SqlMinifier SqlMinifier { get; set; }
@@ -103,7 +191,6 @@ namespace bleak.Sql.VersionManager
             }
         }
     }
-
     public static class StringExtensionMethods
     {
         public static string Minify(this string input)
